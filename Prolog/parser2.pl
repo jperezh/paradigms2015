52fdf39a4a6ca7974d847404e2b7984d68c0ecdf
Jaime Alejandro Perez Heredia <Jaime Alejandro Perez Heredia>
2015-11-04 21:28:01 -0500

    Lets play with the recorrer.

--- Prolog/parser2.pl
+++ Prolog/parser2.pl
@@ -25,21 +25,34 @@ convertirLista(Linea, [Linea]).
 dividir(end_of_file, _, ""):- !.
 dividir(:-, [Izquierda|Derecha], Lista):-
     write("dividir: "), write(Izquierda), write(" con derecha "),writeln(Derecha),
-    
+    flatten(Izquierda,ListaIzquierda),
     arg(1, Derecha, Derechota),
     Derechota=..[Izq|Der],
     
-    write("Iz1: "), write(Izq), write(" con der "),writeln(Der),
-    recorrer(Izq,Der,Lista),
-    write("Al final de dividir la lista es: "),writeln(Lista).
+    write("Iz1: "), write(Izq), write(" con der "),write(Der),
+    write(" con lista: "),writeln(Lista),
+    recorrer(Izq,Der,ListaNueva),
+    append(ListaIzquierda,ListaNueva, Lista),
+    write("!!!!Al final de dividir la lista es: "),writeln(Lista).
 
 recorrer(',', [Cabeza|Cola0], Lista):-
+    write("En recorrer con lista: "),write(Lista),
+    write(" cabeza: "),write(Cabeza),
+    flatten(Lista, ListaN),flatten(Cabeza,CabezaN),
+    append(ListaN,CabezaN,ListaNueva),
+    write(" y lista Nueva: "),writeln(ListaNueva),
     arg(1,Cola0,Cola),
     Cola=..[Cabeza2|Cola2],
     write("Cabeza2: "), write(Cabeza2), write(" cola2 "),write(Cola2),write(" LISTA: "),writeln(Lista),
     arg(1,Cola2,Colota),  
-    recorrer(Cabeza2, Cola2, Lista).
-recorrer(_,_,_):- writeln("YO NO DEBERIA ESTAR ACA"), !.
+    write("Lista: "),writeln(Lista),write("Cabeza: "),writeln(Cabeza),write("ListaNueva"),writeln(ListaNueva),
+    write("Antes de llamar recorrer recursivo con cabeza2: "),write(Cabeza2),
+    write(" cola2: "),write(Cola2),write(" ListaNueva: "),writeln(ListaNueva),
+    recorrer(Cabeza2, Cola2, ListaNueva).
+recorrer(_,_,Lista):-
+	write("Lista: "),writeln(Lista),
+	writeln("CARECULO").
+	
 
 
 dividir3([:-|Args], Args) :-
