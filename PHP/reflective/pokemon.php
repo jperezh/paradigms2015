5b23d83b2d3d483788c41374443a28dbe9f3141f
vagrant <vagrant@local.paradigms>
2015-11-29 18:05:38 +0000

    added pokemon

---
+++ PHP/reflective/pokemon.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * Simple example to show two basic PHP reflective methods
+ * get_class and get_class_methods
+ */
+
+class Pokemon {
+    public  $name;
+}
+
+class Electricity extends Pokemon {
+    public  $type  = "Electricity";
+    public $color = "Yellow"; 
+
+    // Attacks
+    public function thunder() {
+    
+    }
+}
+
+class Pikachu extends Electricity{
+    public $owner;
+
+    public function __contruct($owner) {
+    $this->owner = $owner;
+    }
+
+    // Attacks
+    public function tailWhip() {
+    }
+}
+
+$pikachu = new Pikachu("Ash");
+
+var_dump(get_class($pikachu));
+
+var_dump(get_class_methods($pikachu));
+
